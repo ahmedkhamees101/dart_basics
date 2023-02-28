bb3426466c139c03071ca21af4fb35dfe0e62963
ahmed <akhamess106@gmail.com>
2023-02-28 12:02:11 +0200

    new

--- bin/session 3/program_five.dart
+++ bin/session 3/program_five.dart
@@ -3,16 +3,29 @@
 
 countGeneral(List<int> arr) {
   int counter = 0;
-  int key = arr[0];
-  for (int i = 0; i < arr.length; i++) {
-    if (arr[i] == key) {
+
+  for (int i = 0; i < arr.length-1; i++) {
+    if (arr[i] == arr[i+1]) {
       counter++;
+    }else if(arr[i]>arr[i+1]){
+      break;
     }
+    print("${arr[i]}-->${counter++}");
   }
-    for (int i = 1; i < arr.length; i++) {
+
   }
-  print("$key-->${counter++}");
-}
+
+// int? countGeneral(List<int> arr) {
+//   int counter = 1;
+//   for (int i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] == arr[i + 1]) {
+//       counter++;
+//     } else if (arr[i] > arr[i + 1]) {
+//       break;
+//     }
+//     print('${arr[i]}=>$counter');
+//   }
+// }
 
 void main() {
   List<int> num = [1, 2, 3, 1, 3, 6, 7];
