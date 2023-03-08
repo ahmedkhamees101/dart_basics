c2e2d1a5c3092018b4f87754f655d05c82227f46
dart file <ahmedkhamees101@gmail.com>
2023-03-08 06:45:46 +0200

    new

--- bin/session4/program_seven.dart
+++ bin/session4/program_seven.dart
@@ -3,3 +3,24 @@
 determines how many scores are above or equal to the average and how many scores are below the average.
  Enter a negative number to signify the end of the input. Assume that the maximum number of scores is 100.
  */
+// take an int numbers to calculate the average
+// above average an equal and below
+
+import 'dart:io';
+
+calculateAverage(){
+  stdout.write("Enter your Scores :");
+  List<int> list=[];
+
+  for(int i =0; i<100;i++){
+    int scores =int.parse(stdin.readLineSync()!);
+    if (scores>=0 && scores<=100){
+    print(list);
+  }
+    list.add(scores);
+
+  }}
+
+void main(){
+  calculateAverage();
+}
---
+++ bin/testing/oop_two.dart
@@ -0,0 +1,39 @@
+class Shape{
+   double width;
+   double height;
+  Shape(this.width,this.height);
+  double calculateArea(){return 0;}
+  void draw(){}
+
+}
+class Triangle extends Shape {
+  Triangle(double width, double height):super(width,height);
+  @override
+  double calculateArea() {
+    double x =( width/2)*height;
+return x;
+  }
+  @override
+  void draw() {
+    print("i am triangle ");
+  }
+
+}
+class Rectangle extends Shape{
+  Rectangle(double width,double height):super(width,height);
+  @override
+  double calculateArea(){
+    return width*height;
+  }
+  @override
+  void draw(){
+    print("")
+  }
+}
+
+
+void main(){
+  Triangle t = Triangle(10.5, 5.5);
+print( t.calculateArea()) ;
+  t.draw();
+}
